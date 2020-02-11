494627f3a73fcd6d93333a60df6abe247f2786fb
Slavi Shtipkov <development@shtipkov.com>
2020-02-11 10:27:43 +0200

    Fixed a big issue and successfully regenerated the puzzle data 2D Array with data

--- src/views/crossword-puzzle-view.js
+++ src/views/crossword-puzzle-view.js
@@ -4,62 +4,91 @@ import "@vaadin/vaadin-button";
 import "@vaadin/vaadin-checkbox";
 import "@vaadin/vaadin-radio-button/vaadin-radio-button";
 import "@vaadin/vaadin-radio-button/vaadin-radio-group";
+import nanoid from "nanoid";
 import { connect } from "pwa-helpers";
 
 import { store } from "../redux/store";
-import { generateCrossword } from "../redux/actions";
 
 class CrosswordPuzzleView extends connect(store)(LitElement) {
   static get properties() {
     return {
       words: { type: Array },
-      matrixSize: { type: Number },
-      crosswordData: { type: Array }
+      rowNumber: { type: Number },
+      colNumber: { type: Number },
+      crosswordData: { type: Object }
     };
   }
 
   constructor() {
     super();
     this.words = [];
-    this.matrixSize = 20;
+    this.rowNumber = 20;
+    this.colNumber = 20;
     this.crosswordData = [];
   }
 
-  render() {
-    return html`
-      <style>
-        .crossword-puzzle-wrapper {
-          width: 30rem;
-          height: 30rem;
-          margin: auto;
-          background: var(--grey-background);
-          padding: 1rem;
-        }
+  connectedCallback() {
+    console.log("connected 1");
+    store.subscribe(() => {
+      const inputData = store.getState().enrichedWords;
 
-        .crossword-puzzle {
-          display: flex;
-          flex-direction: column;
-          background: #1676f3;
-          height: 100%;
-        }
+      if (inputData.length > 0) {
+        this.renderCrossword(inputData);
+      }
+    });
+  }
 
-        .crossword-row {
-          display: flex;
-          flex: 1;
-        }
+  renderCrossword(inputData) {
+    console.log(inputData, "asd");
 
-        .crossword-box {
-          flex: 1;
-          margin: 0.1rem;
-          text-align: center;
-          background-color: #fff;
-        }
-      </style>
-      <span @click="${this.renderRows}">offf</span>
-      <div class="crossword-puzzle-wrapper">
-        <div class="crossword-puzzle"></div>
-      </div>
-    `;
+    this.createGrid();
+  }
+
+  createGrid() {
+    let div = document.createElement("div");
+    div.style = "display: flex; flex: 1";
+    let span = document.createElement("span");
+    span.style =
+      "flex: 1; margin: 0.1rem; text-align: center; background-color: #fff; font-size: 1rem; line-hight: 1rem;";
+
+    let crosswordPuzzle = document.createElement("div");
+    crosswordPuzzle.className = "crossword-puzzle";
+    crosswordPuzzle.style =
+      "display: flex; flex-direction: column; background: #1676f3; height: 100%;";
+
+    let crosswordPuzzleWrapper = document.createElement("div");
+    crosswordPuzzleWrapper.className = "crossword-puzzle-wrapper";
+    crosswordPuzzleWrapper.style =
+      "max-width: 80%; max-height: 80%; margin: auto; background: #1a39601a; padding: 1rem;";
+
+    for (let row = 0; row < this.rowNumber; row++) {
+      const rowId = nanoid();
+      let rowCloned = div.cloneNode(true);
+      rowCloned.className = `crossword-row row-${row}`;
+      rowCloned.setAttribute("id", rowId);
+
+      this.crosswordData[row] = Array(this.rowNumber);
+
+      for (let col = 0; col < this.colNumber; col++) {
+        const colId = nanoid();
+        let colCloned = span.cloneNode(true);
+        colCloned.textContent = "S";
+        colCloned.className = `crossword-col col-${col}`;
+        colCloned.setAttribute("id", colId);
+
+        rowCloned.appendChild(colCloned);
+
+        this.crosswordData[row][col] = { id: colId, row: row, col: col };
+      }
+
+      crosswordPuzzle.appendChild(rowCloned);
+    }
+
+    crosswordPuzzleWrapper.appendChild(crosswordPuzzle);
+
+    document
+      .querySelector("crossword-puzzle-view")
+      .shadowRoot.appendChild(crosswordPuzzleWrapper);
   }
 
   renderBoxes(selector) {
@@ -69,54 +98,6 @@ class CrosswordPuzzleView extends connect(store)(LitElement) {
 
       this.shadowRoot.querySelector(selector).appendChild(crosswordBox);
     }
-  }
-
-  renderRows() {
-    for (let i = 0; i < this.matrixSize; i++) {
-      const crosswordRow = document.createElement("div");
-      crosswordRow.className = `crossword-row row-${i}`;
-
-      this.shadowRoot
-        .querySelector(".crossword-puzzle")
-        .appendChild(crosswordRow);
-
-      this.renderBoxes(`.crossword-row.row-${i}`);
-    }
-  }
-
-  renderWord(str) {
-    // Splitting the input word and create boxes for all chars
-    str.split("").forEach(char => console.log(char));
-
-    const crosswordBox = document.createElement("div");
-    crosswordBox.className = "crossword-box";
-    crosswordBox.textContent = char;
-
-    this.shadowRoot
-      .querySelector(".crossword-puzzle")
-      .appendChild(crosswordBox);
-  }
-
-  generateCrossword(e) {
-    store.dispatch(generateCrossword(this.words));
-
-    // tests with fake data
-
-    const matrixSize = 30;
-    const demoPlayground = [...Array(matrixSize)].map((x, j) => {
-      return Array(matrixSize).fill(crosswordBox);
-    });
-    console.log(demoPlayground);
-
-    this.enrichedWords = store.getState().enrichedWords;
-    const fakeData = this.enrichedWords.map(ew => {
-      return {
-        ...ew,
-        startX: 3,
-        startY: 3,
-        direction: "right"
-      };
-    });
   }
 }
 
