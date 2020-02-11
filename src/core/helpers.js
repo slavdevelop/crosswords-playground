8b9db66d1c47fbec5ea07878cf479ea646ecd046
Slavi Shtipkov <development@shtipkov.com>
2020-02-11 11:38:37 +0200

    Refactored helper function to support rowNumber !== colNumber

--- src/core/helpers.js
+++ src/core/helpers.js
@@ -1,7 +1,24 @@
-export const getAppropriateStartingPoint = (matrixSize, word) => {
-  const startingPoint = parseInt(matrixSize / 2) - parseInt(word.length / 2);
+export const getAppropriateStartingPoint = (
+  rowNumber,
+  colNumber,
+  isHorizontal = true,
+  word
+) => {
+  if (rowNumber === colNumber) {
+    const nearCenter = parseInt(rowNumber / 2) - parseInt(word.length / 2);
 
-  return { x: startingPoint, y: startingPoint };
+    return [nearCenter, nearCenter];
+  }
+
+  if (rowNumber < colNumber && isHorizontal) {
+    const horizontalCenter =
+      parseInt(colNumber / 2) - parseInt(word.length / 2);
+    const verticalCenter = parseInt(row / 2);
+
+    return [horizontalCenter, verticalCenter];
+  }
+
+  return [rowNumber / 2 + 1, colNumber / 2 + 1];
 };
 
 export const isStringLengthValid = (str, xSize) =>
