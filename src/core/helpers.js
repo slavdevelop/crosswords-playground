58f18745454ab9f09af17ecde697bb9134c04814
Slavi Shtipkov <development@shtipkov.com>
2020-02-11 10:24:54 +0200

    Refactoring and started using the helpers

--- src/core/helpers.js
+++ src/core/helpers.js
@@ -1,22 +1,8 @@
-import nanoid from "nanoid";
-
 export const getAppropriateStartingPoint = (matrixSize, word) => {
   const startingPoint = parseInt(matrixSize / 2) - parseInt(word.length / 2);
 
   return { x: startingPoint, y: startingPoint };
 };
-
-export const getPlaygroundInstance = matrixSize =>
-  [...Array(matrixSize)].map((element, index) => {
-    const boxArray = [];
-
-    for (let i = 0; i < matrixSize; i++) {
-      const box = { id: nanoid(), row: index, col: i };
-      boxArray.push(box);
-    }
-
-    return boxArray;
-  });
 
 export const isStringLengthValid = (str, xSize) =>
   str.length >= 2 && str.length <= xSize ? true : false;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,3 @@ import "./views/crossword-forms-view";
 import "./views/crossword-puzzle-view";
 
 import "./styles.css";
-
-import { getPlaygroundInstance } from "./core/helpers";
-
-const playground = getPlaygroundInstance(10);
-
-console.log(playground);
